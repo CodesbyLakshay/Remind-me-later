c435d04c9ef927db8b34c482634cd0c06e35550d
lakshayjain <lakshay2007jain@gmail.com>
2025-05-15 13:08:41 +0530

    Initial commit

---
+++ src/main/java/Symplique_Remind_me_later/RemindMeLaterApplication.java
@@ -0,0 +1,13 @@
+package Symplique_Remind_me_later;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindMeLaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindMeLaterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Symplique_Remind_me_later/RemindMeLaterApplicationTests.java
@@ -0,0 +1,13 @@
+package Symplique_Remind_me_later;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindMeLaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
